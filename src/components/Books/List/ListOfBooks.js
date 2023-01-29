bc08fb6e7780dfb429829f988e8367d1c8c9e17c
Marcia Moss <marcia_moss@hotmail.com>
2023-01-29 01:31:58 -0500

    moved the synopsis to expandable

--- src/components/Books/List/ListOfBooks.js
+++ src/components/Books/List/ListOfBooks.js
@@ -2,6 +2,7 @@ import React from "react";
 import { useSelector } from 'react-redux';
 import createBookObject from "../../../utils/createBookObject";
 import Button from "../../Button";
+import ExpandablePanel from '../../ExpandablePanel';
 import { GiBookCover } from 'react-icons/gi';
 
 const ListOfBooks = ({book}) => {
@@ -19,15 +20,17 @@ const ListOfBooks = ({book}) => {
                     : <img className="w-20 h-fit mr-5" src={bookObject.bookimg} alt="NoImageAvailable"/>}
                 </div>
                 <div className="w-4/5 mb-3">
-                    <span className="text-black text-lg text-bold">
-                        <h1 className="text-bold text-xl underline">{bookObject.title} {bookObject.authors}</h1>
+                    <span className="text-lg font-bold">
+                        <h1 className="font-bold text-xl underline">{bookObject.title} {bookObject.authors}</h1>
                             <Button className={`${signedIn ? 'float-left mr-3' : 'mb-2'} mt-3 font-bold text-black border-0 bg-gray-300`}>
                                 <a href={bookObject.booklink} target="_blank" rel="noreferrer">Buy</a>
                             </Button>
                             {signedIn ? 
                                 <Button className="font-bold text-black border-0 mt-3 mb-2 bg-blue-200">Save</Button>
                                 : ''}
-                            {bookObject.synopsis ? <p className="text-bold">{bookObject.synopsis}</p> : ''}
+                            <ExpandablePanel header={<div className="font-bold">Synopsis</div>}>
+                                {bookObject.synopsis ? <p>{bookObject.synopsis}</p> : 'Not Available'}
+                            </ExpandablePanel>
                     </span>
                 </div>
             </div>
