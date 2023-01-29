9929d7e217af84b227bd6f5f29696ee23aaa39b7
Marcia Moss <marcia_moss@hotmail.com>
2023-01-29 17:52:20 -0500

    variable name fix in auth slice

--- src/store/slices/authDataSlice.js
+++ src/store/slices/authDataSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 const initialState = {
   signedIn: false,
-  authDataUserId: null,
+  authUserId: null,
   userName: null,
   showError: false,
   errorMessage: null
