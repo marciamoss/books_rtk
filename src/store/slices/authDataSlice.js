6d7162f30bf8a97c14b324b58abd199ca02b3639
Marcia Moss <marcia_moss@hotmail.com>
2023-01-31 22:20:21 -0500

    removed the authdata reset code

--- src/store/slices/authDataSlice.js
+++ src/store/slices/authDataSlice.js
@@ -15,23 +15,7 @@ const authDataSlice = createSlice({
     authDataInfo(state, action) {
       return { ...state, ...action.payload};
     }
-  },
-  extraReducers: (builder) => {
-    builder.addMatcher(
-      userApi.endpoints.addUser.matchRejected,
-      (state, { payload, meta }) => {
-          return{...state, signedIn: false, authUserId: null, userName: null, showError: true, errorMessage: 'You can still search for books'};
-      }
-    );
-    builder.addMatcher(
-      userApi.endpoints.fetchUser.matchRejected,
-      (state, { payload, meta }) => {
-        if(meta.arg.originalArgs) {
-          return{...state, signedIn: false, authUserId: null, userName: null, showError: true, errorMessage: 'You can still search for books'};
-        }
-      }
-    );
-  },
+  }
 });
 
 export const { authDataInfo } = authDataSlice.actions;
